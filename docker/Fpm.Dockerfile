76975ad3cfa766c3b2670695d015e9361514e8cb
Denis <makarenkodeniz@gmail.com>
2019-11-14 22:54:24 +0100

    frontend

--- docker/Fpm.Dockerfile
+++ docker/Fpm.Dockerfile
@@ -9,18 +9,20 @@ RUN apt-get update && apt-get install -y \
 && curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer \
 && composer --version
 
-RUN apt-get install -y gnupg
-# && apt-get install curl -y && apt-get install apt-transport-https -y
-RUN apt-get install -y build-essential libssl-dev zlib1g-dev libpng-dev libjpeg-dev libfreetype6-dev
+# RUN apt-get install -y gnupg
+
+RUN apt-get install -y gnupg build-essential libssl-dev zlib1g-dev libpng-dev libjpeg-dev libfreetype6-dev
 
 # RUN docker-php-ext-configure gd \
 #     && docker-php-ext-install gd
+
 RUN docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ \
     && docker-php-ext-install gd
 
 RUN curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - && echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 
 RUN apt-get update && apt-get install yarn -y
+
 # && yarn add @symfony/webpack-encore --dev \
 # && yarn encore dev
 
--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -15,22 +15,17 @@ class CategoryController extends AbstractController
 {
     public function showCategories(string $template): Response
     {
-        $getParams = $_GET['get'] ?? null;
-        $categoryId = $subCategoryId = null;
-
-        if(null !== $getParams){
-            $categoryId = intval($getParams['category']) ? $getParams['category'] : $categoryId;
-            $subCategoryId = intval($getParams['subcategory']) ? $getParams['subcategory'] : $subCategoryId;
-        }
-
         $repositoryCategories = $this->getDoctrine()->getRepository(Category::class);
         $repositorySubCategories = $this->getDoctrine()->getRepository(Subcategory::class);
 
+        $categories = [];
+        foreach ($repositoryCategories->findAll() as $category) {
+            $categories[$category->getId()]['cat'] = $category;
+            $categories[$category->getId()]['subcat'] = $repositorySubCategories->findBy(['category' => $category->getId()]);
+        }
+
         return $this->render($template, [
-            'categoryId' => $categoryId,
-            'categories' => $repositoryCategories->findAll(),
-            'subcategoryId' => $subCategoryId,
-            'subcategories' => $repositorySubCategories->findAll(),
+            'categories' => $categories,
 
         ]);
     }
