2cc1061851f9ed5a00ebe3ee0a2705b56bc63f7f
Denis <makarenkodeniz@gmail.com>
2019-11-15 01:18:34 +0100

    Search page

--- assets/js/jquery.instantSearch.js
+++ assets/js/jquery.instantSearch.js
@@ -33,10 +33,6 @@
         itemTemplate: '\
                 <article class="post">\
                     <h2><a href="{{ url }}">{{ title }}</a></h2>\
-                    <p class="post-metadata">\
-                       <span class="metadata"><i class="fa fa-calendar"></i> {{ date }}</span>\
-                       <span class="metadata"><i class="fa fa-user"></i> {{ author }}</span>\
-                    </p>\
                 </article>'
     };
 
